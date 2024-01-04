59fdb2d2be6e7daf739ab7724c3a2b8d704827b6
islam <dujsobaevislam01@gmail.com>
2024-01-04 23:19:12 +0600

    update -> project

--- app/contact/models.py
+++ app/contact/models.py
@@ -3,9 +3,18 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Contact(models.Model):
-    phone_number = PhoneNumberField(max_length=20)
-    telegram_url = models.URLField(blank=True, null=True, verbose_name='Telegram URL', help_text='URL for Telegram.')
-    instagram_url = models.URLField(blank=True, null=True, verbose_name='Instagram URL', help_text='URL for Instagram.')
-
+    phone_number = PhoneNumberField(max_length=20, verbose_name='Номер телефона')
+    telegram_url = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name='Ссылка на Telegram',
+        help_text='URL для Telegram.'
+    )
+    instagram_url = models.URLField(
+        blank=True,
+        null=True,
+        verbose_name='Ссылка на Instagram',
+        help_text='URL для Instagram.'
+    )
     def __str__(self):
         return f"{self.phone_number} - {self.telegram_url or 'No Telegram'} - {self.instagram_url or 'No Instagram'}"
